15c55cd4aa6b615f9621edf17d807e1b5f8042a4
jjspscl <jjspscl@gmail.com>
2025-05-25 01:15:25 +0800

    feat: implement messaging screen and enhance tab navigation with custom buttons

---
+++ mobile/components/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import { ScrollView } from "tamagui";
+
+import { Calendar } from "react-native-calendars";
+const Home = () => {
+    return (
+       <ScrollView>
+
+        <Calendar/>
+       </ScrollView>
+    );
+}
+
+export default Home;
