ebeea322634420a7572e83c61f5a98529773e1d3
jjspscl <jjspscl@gmail.com>
2025-05-25 01:46:51 +0800

    feat: refactor layout and home components for improved structure and styling

--- mobile/components/screens/Home/index.tsx
+++ mobile/components/screens/Home/index.tsx
@@ -1,13 +1,24 @@
 import { ScrollView } from "tamagui";
 
 import { Calendar } from "react-native-calendars";
+import UserManagement from "./user-management";
+import { StyleSheet } from "react-native";
+
 const Home = () => {
     return (
-       <ScrollView>
-
-        <Calendar/>
+       <ScrollView style={styles.container}> 
+        <Calendar />
+        <UserManagement />
        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    calendar: {
+        marginBottom: 20,
+    },
+});
 
 export default Home;
