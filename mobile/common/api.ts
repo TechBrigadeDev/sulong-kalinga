c06842acac495f65bf99d62c0e5c90c6b9ce1c30
jjspscl <jjspscl@gmail.com>
2025-05-26 04:58:33 +0800

    feat: Add Gradle wrapper and configuration for Android project
    - Added gradle-wrapper.properties to specify Gradle distribution URL and settings.
    - Created gradlew and gradlew.bat scripts for executing Gradle tasks on POSIX and Windows systems respectively.
    - Implemented settings.gradle to manage React Native and Expo plugins, including autolinking configurations.
    - Developed BeneficiaryList component to display and manage beneficiaries with view and edit options.
    - Introduced user store using Zustand for managing user state in the application.

--- mobile/common/api.ts
+++ mobile/common/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API URL:", process.env.EXPO_PUBLIC_API_URL);
+
 export const axiosClient = axios.create({
     baseURL: process.env.EXPO_PUBLIC_API_URL,
 })
