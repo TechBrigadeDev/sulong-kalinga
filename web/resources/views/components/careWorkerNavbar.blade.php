89da2a013bd289e6088fdf25bc5d107d091624e9
ryochui <155883482+ryochui@users.noreply.github.com>
2025-05-27 19:16:23 +0800

    D14 Change logo to dashboard on login

--- web/resources/views/components/careWorkerNavbar.blade.php
+++ web/resources/views/components/careWorkerNavbar.blade.php
@@ -7,7 +7,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('landing') }}">
+        <a class="navbar-brand" href="/care-worker/dashboard" >
             <img src="{{ asset('images/cose-logo.png') }}" alt="System Logo" width="30" class="me-2">
             <span class="text-dark">SulongKalinga</span>
         </a>
