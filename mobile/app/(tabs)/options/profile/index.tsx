f79ae8c654b86c68de36793cfdd8d26ed05e7ce6
jjspscl <jjspscl@gmail.com>
2025-05-29 10:55:05 +0800

    Refactor navigation links in Profile component and improve layout styles in BeneficiaryList

---
+++ mobile/app/(tabs)/options/profile/index.tsx
@@ -0,0 +1,18 @@
+import { SafeAreaView, StyleSheet } from "react-native";
+
+const ProfileScreen = () => {
+    return (
+        <SafeAreaView style={style.container}>
+
+
+        </SafeAreaView>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
+
+export default ProfileScreen;
--- mobile/app/(tabs)/options/user-management/beneficiaries/index.tsx
+++ mobile/app/(tabs)/options/user-management/beneficiaries/index.tsx
@@ -38,6 +38,7 @@ const Beneficiaries = () => {
 
 const styles = StyleSheet.create({
     container: {
+        display: "flex",
         marginHorizontal: 20,
     }
 })
