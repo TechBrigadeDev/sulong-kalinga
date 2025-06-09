06639dfced31ff79d40d1c24da5d9f4f2cd83e85
jjspscl <jjspscl@gmail.com>
2025-06-10 05:23:56 +0800

    sk-update-records: enhance care records functionality
    - Added a button to create new care records
    - Removed unused data.json file
    - Updated Tamagui dependencies to version 1.126.14

--- mobile/app/(tabs)/options/reports/care-records/index.tsx
+++ mobile/app/(tabs)/options/reports/care-records/index.tsx
@@ -1,17 +1,37 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { Plus } from "lucide-react-native";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, YStack } from "tamagui";
+import { Button, View, YStack } from "tamagui";
 
 import WCPRecordsList from "~/features/records/wcp/list";
 import WCPRecordsSearch from "~/features/records/wcp/list/search";
 
 const Screen = () => {
+    const router = useRouter();
+
+    const handleCreateRecord = () => {
+        router.push("/care-plan");
+    };
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={styles.container}>
                 <YStack py="$4" gap="$4">
                     <WCPRecordsSearch />
+                    <Button
+                        size="$3"
+                        theme="black"
+                        onPress={
+                            handleCreateRecord
+                        }
+                    >
+                        <Plus
+                            size={16}
+                            color={"white"}
+                        />
+                        Create
+                    </Button>
                 </YStack>
                 <View style={{ flex: 1 }}>
                     <WCPRecordsList />
